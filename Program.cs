2c4328cf8cd17f791ff972ccce968822e4de0134
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-07 18:27:43 +0530

    [ASHIQ] added : TWO_POWER

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+namespace POWER_TWO
+{
+    class PowerofTwo
+    {
+        static void Main()
+        {
+            Console.WriteLine(IsPowerOfTwo(9223372036854775809));
+            Console.WriteLine(IsPowerOfTwo(4));
+            Console.ReadLine();
+        }
+        static bool IsPowerOfTwo(ulong n)
+        {
+            if (n == 0)
+                return false;
+            while (n != 1)
+            {
+                if (n % 2 != 0)
+                    return false;
+                n = n / 2;
+            }
+            return true;
+        }
+    }
+}
+
